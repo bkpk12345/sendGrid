527851701951d1346e4149d6641f9e893c64ce7a
Balkrushna Jadhav <bkpk7745@gmail.com>
2019-05-24 20:25:08 +0530

    added few changes

--- app.js
+++ app.js
@@ -2,23 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const jobs = require('./opp.json')
 const app = express()
-// const cronExec = require('./routes/index')
-const index = require('./routes/index')
-// const change = require('./routes/change')
+const { cronExec } = require('./routes/index')
+
 const mongoose = require('mongoose')
 const conn = require('./config/db')
+
 mongoose
   .connect('mongodb://localhost/oppilio-dev', { useNewUrlParser: true })
   .then(() => {})
-app.use('/api', index)
 
-// cronExec()
-// conn()
-//   .then(() => {
-//     console.log('connected to db')
-//   })
-//   .catch(e => {
-//     console.log('e')
-//   })
+cronExec()
+
 const port = 9000
 app.listen(port, console.log(`listening at ${port}`))
